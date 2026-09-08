5a683c5ba93bde830973ab6c90bfa291568007e6
pc-on-fire-rip <nathpranjal120@gmail.com>
2026-09-08 19:50:52 +0530

    hosting locally

--- client/client/vite.config.js
+++ client/client/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['diverse-judgment-unified-peoplle.trycloudflare.com']
     port: 5174,
   },
 });
